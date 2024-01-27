eb876438ba83553fc809e48bba54f358c64d4887
unknown <rafazef4@gmail.com>
2024-01-26 19:37:30 -0600

    first commit

---
+++ P1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int a,b;
+float resultado;
+int main(){
+    std::cout<<"Escriba valor 1"<<std::endl;
+    std::cin>>a;
+    std::cout<<"Escriba valor 2"<<std::endl;
+    std::cin>>b;
+resultado=(a-b);
+std::cout<<"El resultado de la resta es= "<<" "<<resultado<<std::endl;
+}
---
+++ p2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+float Pc,Pt,Pr;
+float cuad,baserect,alturarect,basetrian,alturatrian;
+int main(){
+    std::cout<<"Perimetro de un cuadrado"<<std::endl;
+    std::cout<<"Coloque el valor de un lado del cuadrado"<<std::endl;
+    std::cin>>cuad;
+    Pc=(cuad*4);
+    std::cout<<"El perimetro del cuadrado es de= "<<Pc<<std::endl;
+    std::cout<<"Perimetro del Rectangulo"<<std::endl;
+    std::cout<<"Coloque el valor de la base"<<std::endl;
+    std::cin>>baserect;
+    std::cout<<"Coloque el valor de la alruta"<<std::endl;
+    std::cin>>alturarect;
+
+
+
+}   
